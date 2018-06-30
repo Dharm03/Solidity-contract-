5dab5608fce8cf5d7b2cb0bfffd5ee50a97178bd
Dharm03 <31887367+Dharm03@users.noreply.github.com>
2018-06-30 18:24:48 +0530

    Add files via upload

---
+++ inbox.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+// get the list of all accounts on ganache
+accounts = await web3.eth.getAccounts();
+
+// contract deployment
+inbox = await new web3.eth.Contract(JSON.parse(interface))
+.deploy({data: bytecode, arguments: ['Hi there!'] })
+.send({ from: accounts[0], gas: '1000000' });
+
+});
+
+describe('inbox', () =>{
+  it('deployes a contract', () =>{
+    console.log(inbox);
+  });
+
+  });
